c7bb395a1a1079f9582e047c21adcf2d728d3335
Karl robeck alferez <karlalferezfx@gmail.com>
2025-06-21 14:06:20 +0800

    Remove unused imports and commented-out configuration code from scrape command

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -4,11 +4,6 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"log"
-	"os"
-
-	"github.com/BurntSushi/toml"
-	"github.com/karlrobeck/golang-scraping-template/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -23,23 +18,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		// config
-		content, err := os.ReadFile("./config.toml")
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		var config internal.SystemConfig
-
-		if _, err = toml.Decode(string(content), &config); err != nil {
-			log.Fatalln(err)
-		}
-
-		// scrape the data
-
-		// save to database
 
 	},
 }
